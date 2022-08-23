129e5e3562ccdedf4c488e9a5d1034a9ea6d40ce
JessicaJulieth <3497@holbertonschool.com>
2022-08-23 17:06:36 -0500

    Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -14,22 +14,22 @@ def minOperations(n):
     # method that calculates the fewest number of operations needed
     # to result in exactly n H characters in the file
     # to write 'n' characters
-    if n =! int:
-        return 0
-    if n == 0
-    return 0
+    if n == 1:
+            return n
+    if n == 0:
+            return 0
 
     N = 0
     H = 1
-    CopyAll = 0
+    copyAll = 0
     paste = 0
     copie = 0
     while H <= n:
         if n % H == 0:
             copyAll += 1
             copie = H
-        else
-        paste += 1
+        else:
+            paste += 1
         N = copyAll + paste
         H += copie
     return N
