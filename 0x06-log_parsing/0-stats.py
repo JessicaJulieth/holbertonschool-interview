2c916a5af3cc963e2e276d3450247db467805062
JessicaJulieth <3497@holbertonschool.com>
2022-09-15 10:37:30 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,39 +3,38 @@
 Write a script that reads stdin line by line and computes metrics:
 """
 import sys
-import signal
 
 
 def metrics(codes, size):
     """
     Function that print stats
     """
-    print("File size: {}".format(codes))
-    for key, value in sorted(size.items()):
+    print("File size: {}".format(size))
+    for key, value in sorted(codes.items()):
         if value > 0:
             print("{}: {}".format(key, value))
 
 
-if __name__ == '__main__':
-    x = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-    count = 1
-    z = 0
+x = {'200': 0, '301': 0, '400': 0, '401': 0,
+     '403': 0, '404': 0, '405': 0, '500': 0}
+z = 0
+count = 1
 
 try:
     for line in sys.stdin:
         split_args = line.split()
-        if len(split_args) > 6:
-            status = split_args[-2]
-            file_size = split_args[-1]
-            z += int(file_size)
-            if status in x:
-                count += 1
-                x[status] += 1
-                if count % 10 == 0:
-                    metrics(z, x)
+        if len(split_args) < 7:
+            continue
+        z += int(split_args[-1])
+        code_stats = split_args[-2]
+        if code_stats in x:
+            x[code_stats] += 1
+        if count == 10:
+            metrics(x, z)
+            count = 1
+        else:
+            count += 1
+    metrics(x, z)
 except KeyboardInterrupt:
-    metrics(z, x)
+    metrics(x, z)
     raise
-else:
-    metrics(z, x)
