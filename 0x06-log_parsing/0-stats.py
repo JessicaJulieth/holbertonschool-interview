d8b0b5c2712320cf95fd88d6f0b981c28293ea42
JessicaJulieth <3497@holbertonschool.com>
2022-09-15 10:14:06 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,6 +3,7 @@
 Write a script that reads stdin line by line and computes metrics:
 """
 import sys
+import signal
 
 
 def metrics(codes, size):
@@ -16,23 +17,25 @@ def metrics(codes, size):
 
 
 if __name__ == '__main__':
+    z = 0
     x = {'200': 0, '301': 0, '400': 0, '401': 0,
          '403': 0, '404': 0, '405': 0, '500': 0}
-    count = 1
-    z = 0
 
 try:
+    count = 1
     for line in sys.stdin:
         split_args = line.split()
         if len(split_args) > 6:
-            status = split_args[-2]
+            size = split_args[-2]
             file_size = split_args[-1]
             z += int(file_size)
-            if status in x:
+            if size in x:
                 count += 1
-                x[status] += 1
+                x[size] += 1
                 if count % 10 == 0:
                     metrics(z, x)
 except KeyboardInterrupt:
-    metrics(x, z)
+    metrics(z, x)
     raise
+else:
+    metrics(z, x)
