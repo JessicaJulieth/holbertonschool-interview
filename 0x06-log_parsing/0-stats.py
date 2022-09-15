d804be25b0d34352a1d3f80af1c217e6e5ece8ef
JessicaJulieth <3497@holbertonschool.com>
2022-09-15 09:26:17 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,24 +6,25 @@ import sys
 
 
 def metrics(codes, size):
-    print("File size: {}".format(size))
-    for key, value in sorted(codes.items()):
-        if value > 0:
+    print("File size: {}".format(codes))
+    for key, value in sorted(size.items()):
+        if value != 0:
             print("{}: {}".format(key, value))
 
 
-x = {'200': 0, '301': 0, '400': 0, '401': 0,
-     '403': 0, '404': 0, '405': 0, '500': 0}
-z = 0
-count = 1
+if __name__ == '__main__':
+    x = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+    z = 0
 
 try:
+    count = 1
     for line in sys.stdin:
-        split_line = line.strip().split()
-        if len(split_line) < 7:
+        split_args = line.strip().split()
+        if len(split_args) < 7:
             continue
-        z += int(split_line[-1])
-        code_stats = split_line[-2]
+        z += int(split_args[-1])
+        code_stats = split_args[-2]
         if code_stats in x:
             x[code_stats] += 1
         if count == 10:
