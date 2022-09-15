db964154d96a8c4ec5dbce0bc40942b46cfe1703
JessicaJulieth <3497@holbertonschool.com>
2022-09-15 10:01:50 -0500

    Log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,32 +6,33 @@ import sys
 
 
 def metrics(codes, size):
-    print("File size: {}".format(size))
-    for key, value in sorted(codes.items()):
+    """
+    Function that print stats
+    """
+    print("File size: {}".format(codes))
+    for key, value in sorted(size.items()):
         if value > 0:
             print("{}: {}".format(key, value))
 
 
-x = {'200': 0, '301': 0, '400': 0, '401': 0,
-     '403': 0, '404': 0, '405': 0, '500': 0}
-count = 1
-z = 0
+if __name__ == '__main__':
+    x = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+    count = 1
+    z = 0
 
 try:
     for line in sys.stdin:
         split_args = line.split()
-        if len(split_args) < 7:
-            continue
-        z += int(split_args[-1])
-        code_stats = split_args[-2]
-        if code_stats in x:
-            x[code_stats] += 1
-        if count == 10:
-            metrics(x, z)
-            count = 1
-        else:
-            count += 1
-    metrics(x, z)
+        if len(split_args) > 6:
+            status = split_args[-2]
+            file_size = split_args[-1]
+            z += int(file_size)
+            if status in x:
+                count += 1
+                x[status] += 1
+                if count % 10 == 0:
+                    metrics(z, x)
 except KeyboardInterrupt:
     metrics(x, z)
     raise
