270f610ad06825139d23b1517f6d98a7f86af8e0
JessicaJulieth <3497@holbertonschool.com>
2022-11-03 19:20:22 -0500

    Other way

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -4,109 +4,65 @@ The N queens puzzle is the challenge of placing N non-attacking
 queens on an N×N chessboard. Write a program that solves the N queens
 problem.
 """
+
 if __name__ == "__main__":
     import sys
 
+    argv = sys.argv
+    argc = len(argv)
 
-def flldiagonals(rest, n):
-    flldiagonals = []
-    for i in rest:
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x -= 1
-            y -= 1
-
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x -= 1
-            y += 1
-
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x += 1
-            y -= 1
-
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x += 1
-            y += 1
-
-    return flldiagonals
-
-
-def safe(x, y, rest, n):
-    """
-    Here safe a position
-    """
-    for _x in range(n):
-        if [_x, y] in rest:
-            return False
-
-    return not [x, y] in flldiagonals(rest, n)
-
-
-def iter(n):
-    """
-    Iterate all positions
-    """
-    rest = []
-    x = 0
-    y = 0
-
-    while x < n:
-        while y < n:
-            if safe(x, y, rest, n):
-                rest.append([x, y])
-                break
-            y += 1
-
-        if len(rest) != (x + 1):
-            x -= 1
-            if x < 0:
-                break
-            y = rest[x][1] + 1
-            del rest[x]
-            continue
-        elif len(rest) == n:
-            print(rest)
-            y += 1
-            del rest[x]
-            continue
-        x += 1
-        y = 0
-
-
-def init():
-    """
-    N queens
-    """
-    argc = sys.argv
-
-    if argc != 2:
-        print("Usage: nqueens N")
+    if argc is not 2:
+        print('Usage: nqueens N')
         exit(1)
 
     try:
-        int(args[1])
-    except User.DoesNotExist:
-        print("N must be a number")
-        sys.exit(1)
+        n = int(argv[1])
+        if n < 4:
+            print('N must be at least 4')
+            exit(1)
+    except ValueError:
+        print('N must be a number')
+        exit(1)
 
-    if int(args[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    z = []
 
-    n = int(args[1])
-    iter(n)
+    def safe(z, row, col):
+        x = []
+        y = []
+        x_diag = []
+        y_diag = []
+
+        for pos in z:
+            x.append(pos[0])
+            y.append(pos[1])
+            x_diag.append(pos[0] + pos[1])
+            y_diag.append(pos[1] - pos[0])
+        if row in x or col in y:
+            return False
+
+        if row + col in x_diag or col - row in y_diag:
+            return False
+
+        return True
+
+    def queens(z, col, arrays=[]):
+        """
+        Queens
+        """
+        for i in range(n):
+            if safe(z, i, col):
+                z.append([i, col])
+                if col == n - 1:
+                    arrays.append(z.copy())
+                    del z[-1]
+                else:
+                    queens(z, col + 1)
+
+        if len(z):
+            del z[-1]
+        return arrays
+
+    z = queens(z, 0)
+
+    for pos in z:
+        print(pos)
