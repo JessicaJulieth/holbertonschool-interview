50756b81ca1e8ddec6ffef8b103a9c975f1622cf
JessicaJulieth <3497@holbertonschool.com>
2022-11-05 22:45:13 -0500

    Changesx

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,71 +1,69 @@
 #!/usr/bin/python3
-"""
-The N queens puzzle is the challenge of placing N non-attacking
+""" The N queens puzzle is the challenge of placing N non-attacking
 queens on an N.N chessboard. Write a program that solves the N queens
 problem.
 """
+
 
 import sys
 
 
 if __name__ == "__main__":
-
-    argv = sys.argv
-    argc = len(sys.argv)
-
-    if argc == 1 or len(sys.argv) > 2:
-        print('Usage: nqueens N')
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
         sys.exit(1)
     N = sys.argv[1]
     try:
-        In = int(N)
+        Ni = int(N)
     except ValueError:
-        print('N must be a number')
+        print("N must be a number")
         sys.exit(1)
-    if In < 4:
-        print('N must be at least 4')
+    if Ni < 4:
+        print("N must be at least 4")
         sys.exit(1)
 
     z = []
 
     def isSafe(z, row, col):
-        x = []
-        y = []
-        x_diag = []
-        y_diag = []
+        """ Checks if queen can be placed in coord of board.
+        Returns True if can, else False
+        """
+        rows = []
+        cols = []
+        diag_r = []
+        diag_l = []
 
-        for pos in z:
-            x.append(pos[0])
-            y.append(pos[1])
-            x_diag.append(pos[0] + pos[1])
-            y_diag.append(pos[1] - pos[0])
+        for square in z:
+            rows.append(square[0])
+            cols.append(square[1])
+            diag_r.append(square[0] + square[1])
+            diag_l.append(square[1] - square[0])
 
-        if row in x or col in y:
+        if row in rows or col in cols:
             return False
-
-        if row + col in x_diag or col - row in y_diag:
+        if row + col in diag_r or col - row in diag_l:
             return False
 
         return True
 
-    def Nqueens(z, col, arrays=[]):
+    def solveNqueens(z, col, arrays=[]):
+        """ Creates array of queen positions
+        Returns array
         """
-        Queens
-        """
-        for i in range(In):
-            if isSafe(z, i, col):
-                z.append([i, col])
-                if col == In - 1:
+        for x in range(Ni):
+            if isSafe(z, x, col):
+                z.append([x, col])
+                if col == Ni - 1:
                     arrays.append(z.copy())
                     del z[-1]
                 else:
-                    Nqueens(z, col + 1)
+                    solveNqueens(z, col + 1)
 
         if len(z):
             del z[-1]
         return arrays
 
-    z = Nqueens(z, 0)
+    z = solveNqueens(z, 0)
 
-    for pos in z:
-        print(pos)
+    for squares in z:
+        print(squares)
