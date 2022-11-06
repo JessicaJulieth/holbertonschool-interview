56e991f1aa349b09d7c64babdd1b07e73732df8f
JessicaJulieth <3497@holbertonschool.com>
2022-11-05 22:03:30 -0500

    Firs way

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -8,109 +8,64 @@ problem.
 import sys
 
 
-def flldiagonals(rest, n):
-    flldiagonals = []
-    for i in rest:
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x -= 1
-            y -= 1
+if __name__ == "__main__":
 
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x -= 1
-            y += 1
+    argv = sys.argv
+    argc = len(sys.argv)
 
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x += 1
-            y -= 1
+    if argc is not 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    N = sys.argv[1]
+    try:
+        I = int(N)
+        if I < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
 
-        x = i[0]
-        y = i[1]
-        while y >= 0 and y < n and x >= 0 and x < n:
-            if [x, y] not in flldiagonals:
-                flldiagonals.append([x, y])
-            x += 1
-            y += 1
+    z = []
 
-    return flldiagonals
+    def isSafe(z, row, col):
+        x = []
+        y = []
+        x_diag = []
+        y_diag = []
 
+        for pos in z:
+            x.append(pos[0])
+            y.append(pos[1])
+            x_diag.append(pos[0] + pos[1])
+            y_diag.append(pos[1] - pos[0])
 
-def safe(x, y, rest, n):
-    """
-    Here safe a position
-    """
-    for _x in range(n):
-        if [_x, y] in rest:
+        if row in x or col in y:
             return False
 
-    return not [x, y] in flldiagonals(rest, n)
+        if row + col in x_diag or col - row in y_diag:
+            return False
 
+        return True
 
-def iter(n):
-    """
-    Iterate all positions
-    """
-    rest = []
-    x = 0
-    y = 0
+    def queens(z, col, arrays=[]):
+        """
+        Queens
+        """
+        for i in range(I):
+            if isSafe(z, i, col):
+                z.append([i, col])
+                if col == I - 1:
+                    arrays.append(z.copy())
+                    del z[-1]
+                else:
+                    queens(z, col + 1)
 
-    while x < n:
-        while y < n:
-            if safe(x, y, rest, n):
-                rest.append([x, y])
-                break
-            y += 1
+        if len(z):
+            del z[-1]
+        return arrays
 
-        if len(rest) != (x + 1):
-            x -= 1
-            if x < 0:
-                break
-            y = rest[x][1] + 1
-            del rest[x]
-            continue
-        elif len(rest) == n:
-            print(rest)
-            y += 1
-            del rest[x]
-            continue
-        x += 1
-        y = 0
+    z = queens(z, 0)
 
-
-def init():
-    """
-    N queens
-    """
-    args = sys.argv
-
-    if len(args) is not 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        int(args[1])
-    except User.DoesNotExist:
-        print("N must be a number")
-        sys.exit(1)
-
-    if int(args[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    n = int(args[1])
-    iter(n)
-
-
-if __name__ == "__main__":
-    init()
+    for pos in z:
+        print(pos)
