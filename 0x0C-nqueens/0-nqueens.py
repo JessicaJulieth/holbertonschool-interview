275055574b2f7c5d25f693b0762c0146d7222ee6
JessicaJulieth <3497@holbertonschool.com>
2022-11-05 22:25:32 -0500

    Changes

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 The N queens puzzle is the challenge of placing N non-attacking
-queens on an N×N chessboard. Write a program that solves the N queens
+queens on an N.N chessboard. Write a program that solves the N queens
 problem.
 """
 
