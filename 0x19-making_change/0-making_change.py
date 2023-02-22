ce1a9918068a50f8b92b2366d3030b4f96e1b120
JessicaJulieth <3497@holbertonschool.com>
2023-02-22 16:18:04 -0500

    Add

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values, determine the fewest
+number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """Return the least number of coins need to return the total
+    @coins: list of the coins available
+    @total: If total is 0 or less, return 0
+    Return: fewest number of coins needed to meet total
+    """
+    n_coins = 0
+    if total <= 0:
+        return 0
+    for coin in sorted(coins)[::-1]:
+        n_coins += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return n_coins
