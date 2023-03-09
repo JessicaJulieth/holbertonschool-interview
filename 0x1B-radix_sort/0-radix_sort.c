bcd26545022b1f14fc81b8d4b9d0f11491417fc7
JessicaJulieth <3497@holbertonschool.com>
2023-03-09 09:47:07 -0500

    Add

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+/**
+ * radix_lsd - Sort array.
+ * @array: Array.
+ * @size: Size.
+ * @div: Divisor.
+ * @minNum: Minimun number
+ */
+void radix_lsd(int *array, int size, int div, int minNum)
+{
+	int i;
+	int bucket[10] = {0}, bucketIDX;
+	int *output;
+
+	output = malloc(sizeof(int) * size);
+	if (!output)
+		return;
+	for (i = 0; i < size; i++)
+		bucket[(array[i] - minNum) / div % 10]++;
+
+	for (i = 1; i < 10; i++)
+		bucket[i] += bucket[i - 1];
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		bucketIDX = (array[i] - minNum) / div % 10;
+		output[--bucket[bucketIDX]] = array[i];
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(output);
+}
+
+/**
+ * radix_sort - Sort array.
+ * @array: Array.
+ * @size: Size.
+ */
+void radix_sort(int *array, size_t size)
+{
+	size_t i;
+	int maxNum, minNum;
+
+	if (!array || !size || size <= 1)
+		return;
+
+	maxNum = array[0];
+	minNum = array[0];
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > maxNum)
+			maxNum = array[i];
+		if (array[i] < minNum)
+			minNum = array[i];
+	}
+	for (i = 1; (maxNum - minNum) / i > 0; i *= 10)
+	{
+		radix_lsd(array, size, i, minNum);
+		print_array(array, size);
+	}
+}
